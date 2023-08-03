197e014f9f70b6bfa4b62684daf543a142091c16
skpub <satodeyannsu@gmail.com>
2023-08-03 23:57:37 +0900

    first

---
+++ meigen_app/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class User(models.Model):
+    name = models.CharField(max_length=128)
+    hspw = models.UUIDField()
+    class Meta:
+        db_table = 'user'
+
+class UserGroup(models.Model):
+    group_id = models.BigIntegerField()
+    user_id = models.BigIntegerField()
+    class Meta:
+        db_table = 'user_group'
+
+class Group(models.Model):
+    name = models.CharField(max_length=128)
+    class Meta:
+        db_table = 'group'
+
+class Meigen(models.Model):
+    meigen = models.CharField(max_length=1024)
+    group_id = models.BigIntegerField()
+    class Meta:
+        db_table = 'meigen'
