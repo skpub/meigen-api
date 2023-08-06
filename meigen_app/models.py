486d5f4adafb3c29b564e4c6282345071448063f
skpub <satodeyannsu@gmail.com>
2023-08-06 22:58:24 +0900

    DBのテーブルに名言発言者(アカウントではなく名言管理グループで管理する対象である発言者たち)を追加した。

--- meigen_app/models.py
+++ meigen_app/models.py
@@ -3,22 +3,18 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=128)
     hspw = models.CharField(max_length=128)
-    class Meta:
-        db_table = 'user'
-
-class UserGroup(models.Model):
-    group_id = models.BigIntegerField()
-    user_id = models.BigIntegerField()
-    class Meta:
-        db_table = 'user_group'
 
 class Group(models.Model):
     name = models.CharField(max_length=128) 
-    class Meta:
-        db_table = 'group'
+
+class Meigener(models.Model):
+    name = models.CharField(max_length=128)
+    group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+class UserGroup(models.Model):
+    group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Meigen(models.Model):
     meigen = models.CharField(max_length=1024)
-    group_id = models.BigIntegerField()
-    class Meta:
-        db_table = 'meigen'
+    meigener = models.ForeignKey(Meigener, on_delete=models.CASCADE)
